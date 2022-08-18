e9a1aba0a4747c4018dad84ef832bc97b904c0ee
Siham29 <siham.mutasem@gmail.com>
2022-08-18 10:46:54 +0300

    Add and Delete user in different component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,5 +15,33 @@ interface User {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
+    public NewUser:User={name:'',age:0,password:'',email:'',userName:''};
+   public UserList:User[]=[
+    {
+      name:'Siham', age:22, password:'12345',email:'siham.m@gmail.com ',userName:'Siham29'
+  
+    },
+  
+    {
+      name:'Israa', age:22, password:'12345',email:'Israa.m@gmail.com ',userName:'Israa55'
+  
+    },
+    {
+      name:'Aya', age:22, password:'12345',email:'Aya.m@gmail.com ',userName:'Ayaa'
+  
+    }
+  
+      
+    ];
+    DeleteUser(user:User){
+    let i =this.UserList.indexOf(user);
+    if(i>=0){
+      this.UserList.splice(i,1);
+    }
+    
+      }
+      addUser(user: User){
+      
+        this.UserList.push(user);
+      }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
 interface User {
   name: string;
@@ -14,46 +14,24 @@ interface User {
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
-  @ Input () public NewUser:User={name:'',age:0,password:'',email:'',userName:''};
-  @ Input () public UserList:User[]=[
-    {
-      name:'Siham', age:22, password:'12345',email:'siham.m@gmail.com ',userName:'Siham29'
-  
-    },
-  
-    {
-      name:'Israa', age:22, password:'12345',email:'Israa.m@gmail.com ',userName:'Israa55'
-  
-    },
-    {
-      name:'Aya', age:22, password:'12345',email:'Aya.m@gmail.com ',userName:'Ayaa'
-  
-    }
-  
-      
-    ];
+ @Output()user=new EventEmitter<User>();
+ @Input() public NewUser:User={name:'',age:0,password:'',email:'',userName:''};
 
   constructor() { }
 
   ngOnInit(): void {
    
   }
+  
+      addUser(form:NgForm){
+        if(!form.form.valid){
+          form.form.markAllAsTouched();
+        }
+        if(form.form.valid)
+        this.user.emit(form.form.value);
+      }
 
 
-  DeleteUser(user:User){
-let i =this.UserList.indexOf(user);
-if(i>=0){
-  this.UserList.splice(i,1);
-}
-
-  }
-  addUser(form:NgForm){
-    if(!form.form.valid){
-      form.form.markAllAsTouched();
-    }
-    if(form.form.valid)
-    this.UserList.push({...this.NewUser});
-  }
 
 
 }
